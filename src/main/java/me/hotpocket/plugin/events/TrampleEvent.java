0a0e5d8c42341d195401674b0d3963bbe188e68e
hotpocket184 <80357042+hotpocket184@users.noreply.github.com>
2022-02-04 11:38:02 -0500

    Updated config.yml

--- src/main/java/me/hotpocket/plugin/events/TrampleEvent.java
+++ src/main/java/me/hotpocket/plugin/events/TrampleEvent.java
@@ -6,25 +6,31 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class TrampleEvent implements Listener {
 
     @EventHandler
     private void onTrample(PlayerInteractEvent event) {
-        if (event.getClickedBlock().getType().equals(Material.SOIL)) {
-            if(event.getAction().equals(Action.PHYSICAL)) {
-                event.setCancelled(true);
-                if (ConfigUtil.getBoolean("message-when-trampled")) {
-                    MessageUtil.playerMessage(event.getPlayer(), ConfigUtil.getString("trample-message"));
-                }
-                if (ConfigUtil.getBoolean("title-when-trampled")) {
-                    MessageUtil.playerTitle(event.getPlayer(), ConfigUtil.getString("trample-title"), ConfigUtil.getString("trample-subtitle"), 0, 30, 5);
-                }
-                if (ConfigUtil.getBoolean("action-bar-when-trampled")) {
-                    MessageUtil.playerActionBar(event.getPlayer(), ConfigUtil.getString("trample-action-bar"));
-                }
+        if (event.getClickedBlock().getType().equals(Material.SOIL) && event.getAction().equals(Action.PHYSICAL)) {
+            event.setCancelled(true);
+            if (ConfigUtil.getBoolean("message-when-trampled")) {
+                MessageUtil.playerMessage(event.getPlayer(), ConfigUtil.getString("trample-message"));
             }
+            if (ConfigUtil.getBoolean("title-when-trampled")) {
+                MessageUtil.playerTitle(event.getPlayer(), ConfigUtil.getString("trample-title"), ConfigUtil.getString("trample-subtitle"), 0, 30, 5);
+            }
+            if (ConfigUtil.getBoolean("action-bar-when-trampled")) {
+                MessageUtil.playerActionBar(event.getPlayer(), ConfigUtil.getString("trample-action-bar"));
+            }
+        }
+    }
+
+    @EventHandler
+    private void onMobTrample(EntityInteractEvent event) {
+        if (event.getBlock().getType().equals(Material.SOIL)) {
+            event.setCancelled(true);
         }
     }
 }
